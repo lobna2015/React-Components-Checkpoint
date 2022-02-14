b448c44b6686a8d79a200bf51fadc63fba9bdecf
LobnaDRISSI <lobnadrissi2020@gmail.com>
2022-02-14 12:16:27 +0100

    first commit

---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const ProfilePhoto = () => {
+  return (
+    <div>
+<h1>CV Lobna DRISSI</h1>
+<img src="/cv.png" alt="cv"/>
+
+    </div>
+  )
+}
+
+export default ProfilePhoto
